d2a706c50e87c1211ec0003f8a276773c13ceb35
Quentin Delamarre <quentin.delamarre8@etu.univ-lorraine.fr>
2017-11-21 11:51:20 +0100

    Can add a new task and modify her name

--- js/app.js
+++ js/app.js
@@ -20,17 +20,48 @@ window.TaskManager = (() => {
         }
 
         display_name(){
-            return $('<span>').addClass('name').text(this.name);
+            let name = $('<span>').addClass('name').text(this.name);
+            let field = $('<input>').prop('type','text');
+            let button = $('<input>').prop('type', 'submit');
+            let editor = $('<form>').append(field).append(button);
+
+            let in_edit = false ;
+
+            let task = this;
+
+            name.click((event) => {
+                event.stopPropagation();
+                event.preventDefault();
+
+                let target = $(event.target);
+
+                if(!in_edit && target.is('span')) {
+                    name.empty();
+                    name.append(editor);
+                    in_edit = true ;
+                }
+
+                if (target.is('input') && target.prop('type')=== 'submit'){
+                    task.tags = field.val();
+                    name.empty();
+                    name.text(task.tags);
+                    in_edit = false;
+                }
+            });
+
+            return name;
         }
 
         display_duration(){
             let item = $('<li>').addClass('duration').text(this.duration);
+            
             if(this.duration <=10) {
                 item.addClass('short');
             }
             else if (this.duration >= 20){
                 item.addClass('long');
             }
+
             return item ;
         }
 
@@ -64,8 +95,7 @@ window.TaskManager = (() => {
                     item.text(task.tags);
                     in_edit = false;
                 }
-
-;            })
+            });
             return item ;
         }
 
@@ -74,12 +104,20 @@ window.TaskManager = (() => {
     module.tasks = [];
 
     module.display_tasks = (div_id) => {
+        $(div_id).empty();
         let container = $("<ul>").prop('id','tasks');
         $(div_id).append(container);
 
         for(let task of module.tasks){
             $(container).append(task.display_item());
         }
+    };
+
+    module.add_item = () => {
+        $('#add').click(function(){
+            TaskManager.tasks.push(new TaskManager.Task());
+            TaskManager.display_tasks('#taskmanager');
+        })
     };
 
     return module ;
@@ -93,7 +131,7 @@ $(() => {
     TaskManager.tasks.push(new TaskManager.Task('tache 1', 10, 'test'));
     TaskManager.tasks.push(new TaskManager.Task('tache 2', 20, 'test2'));
     TaskManager.tasks.push(new TaskManager.Task('tache 3', 15, 'test1'));
-
     TaskManager.display_tasks('#taskmanager');
+    TaskManager.add_item();
 
 });
