fe2512e7eceb6e31adee3ec33d7afca09e0208f9
Islam Elshobokshy <islam20088@hotmail.com>
2017-11-28 10:55:05 +0100

    Added bootstrap to project

--- client/js/app.js
+++ client/js/app.js
@@ -10,7 +10,7 @@ window.TaskManager = (() => {
         }
 
         display_item(){
-            let properties = $('<ul>');
+            let properties = $('<div>').addClass('row');
             properties.append(this.display_duration());
             properties.append(this.display_category());
             return $('<li>')
@@ -20,7 +20,11 @@ window.TaskManager = (() => {
         }
 
         display_name(){
-            let name = $('<span>').addClass('name').text(this.name);
+            let name = $('<span>').addClass('col-md-12 name').text(this.name + ' ');
+            let editBtn = $('<i>').addClass('fa fa-pencil').prop('aria-hidden','true')
+
+            name.append(editBtn);
+
             let field = $('<input>').prop('type','text');
             let button = $('<input>').prop('type', 'submit');
             let editor = $('<form>').append(field).append(button);
@@ -35,16 +39,17 @@ window.TaskManager = (() => {
 
                 let target = $(event.target);
 
-                if(!in_edit && target.is('span')) {
+                if(!in_edit && target.is('i')) {
                     name.empty();
                     name.append(editor);
-                    in_edit = true ;
+                    in_edit = true;
                 }
 
                 if (target.is('input') && target.prop('type')=== 'submit'){
                     task.tags = field.val();
                     name.empty();
                     name.text(task.tags);
+                    name.append(editBtn);
                     in_edit = false;
                 }
             });
@@ -53,7 +58,11 @@ window.TaskManager = (() => {
         }
 
         display_duration(){
-            let item = $('<li>').addClass('duration').text(this.duration);
+            let item = $('<p>').addClass('col-md-6 text-center duration').text(' duration = ' + this.duration);
+
+            item.prepend(
+                $('<i>').addClass('fa fa-clock-o').prop('aria-hidden','true')
+            );
             
             if(this.duration <=10) {
                 item.addClass('short');
@@ -66,7 +75,11 @@ window.TaskManager = (() => {
         }
 
         display_category(){
-            let item = $('<li>').addClass('category').text(this.tags);
+            let item = $('<p>').addClass('col-md-6 text-center category').text(this.tags + ' ');
+            let editBtn = $('<i>').addClass('fa fa-pencil').prop('aria-hidden','true')
+
+            item.append(editBtn);
+
             let field = $('<input>').prop('type','text');
             let button = $('<input>').prop('type', 'submit');
             let editor = $('<form>').append(field).append(button);
@@ -75,24 +88,23 @@ window.TaskManager = (() => {
 
             let task = this;
 
-
-
             item.click((event) => {
                 event.stopPropagation();
                 event.preventDefault();
 
                 let target = $(event.target);
 
-                if(!in_edit && target.is('li')) {
+                if(!in_edit && target.is('i')) {
                     item.empty();
                     item.append(editor);
-                    in_edit = true ;
+                    in_edit = true;
                 }
 
-                if (target.is('input') && target.prop('type')=== 'submit'){
+                if (target.is('input') && target.prop('type') === 'submit'){
                     task.tags = field.val();
                     item.empty();
                     item.text(task.tags);
+                    item.append(editBtn);
                     in_edit = false;
                 }
             });
